baaf27a58076ec7bef2934fd037348c8fd90a004
Nanang <nanangkhilmi714@gmail.com>
2023-01-12 14:26:03 -0400

    menambahkan umur

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.ujianandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.ujianandroid.R.id;
+import static com.example.ujianandroid.R.layout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -16,28 +18,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
 
-        EditText edNamaDepan = (EditText) findViewById(R.id.edNamaDepan);
-        EditText edNamaBelakang = (EditText) findViewById(R.id.edNamaBelakang);
-        Button btnSimpan = (Button) findViewById(R.id.btnSimpan);
+        EditText edNamaDepan = (EditText) findViewById(id.edNamaDepan);
+        EditText edNamaBelakang = (EditText) findViewById(id.edNamaBelakang);
+        EditText edUmur = (EditText) findViewById(id.edUmur);
+        Button btnSimpan = (Button) findViewById(id.btnSimpan);
 
         ArrayList<String> daftar_nama = new ArrayList<>();
 
         Intent intent_list = new Intent(MainActivity.this, ListActivity.class);
 
-        btnSimpan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String isian_nama_depan = edNamaDepan.getText().toString();
-                String isian_nama_belakang = edNamaBelakang.getText().toString();
+        btnSimpan.setOnClickListener(view -> {
+            String isian_nama_depan = edNamaDepan.getText().toString();
+            String isian_nama_belakang = edNamaBelakang.getText().toString();
+            String isian_umur = edUmur.getText().toString();
 
-                if(isian_nama_depan.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
-                    daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
+            if(isian_nama_depan.isEmpty() && isian_nama_belakang.isEmpty() && isian_umur.isEmpty()){
+                Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
+            }else{
+                int counter = Integer.parseInt(isian_umur);
+                String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang).concat(", ").concat("Status : ");
+                daftar_nama.clear();
+                for(Integer i = 1; i <= counter; i++) {
+                    if(counter <= 10) {
+                        daftar_nama.add( i + " " +  nama_lengkap.concat("Anak"));
+                    } else if(counter <= 20) {
+                        daftar_nama.add( i + " " +  nama_lengkap.concat("Remaja"));
+                    } else if(counter <= 40) {
+                        daftar_nama.add( i + " " +  nama_lengkap.concat("Dewasa"));
+                    } else {
+                        daftar_nama.add( i + " " +  nama_lengkap.concat("Tua"));
+                    }
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
